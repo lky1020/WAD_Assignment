7988f6d392ec40708b53f21b4cc5770acf0ca25a
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-02-08 09:06:12 +0800

    Modify structure of web

--- WAD_Assignment/Homepage.aspx
+++ WAD_Assignment/Homepage.aspx
@@ -1,5 +1,17 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/WAD.Master" AutoEventWireup="true" CodeBehind="Homepage.aspx.cs" Inherits="WAD_Assignment.Homepage" %>
 
 <asp:Content ID="Homepage" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    Homepage
+    <section class="banner" id="banner">
+        <div class="content">
+            <h2>Always Choose Good</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur 
+                adipisicing elit. Beatae, tempora modi 
+                laudantium blanditiis tenetur labore 
+                maiores at dolore eligendi doloremque! 
+                Fugiat sed atque repudiandae quaerat 
+                esse ipsum, accusantium deleniti quia.
+            </p>
+            <a href="#" class='btn'>Our Menu</a>
+        </div>
+    </section>
 </asp:Content>
--- WAD_Assignment/WAD.Master.designer.cs
+++ WAD_Assignment/WAD.Master.designer.cs
@@ -15,15 +15,6 @@ namespace WAD_Assignment
     {
 
         /// <summary>
-        /// head control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -31,33 +22,6 @@ namespace WAD_Assignment
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// Menu1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
-
-        /// <summary>
-        /// SiteMapDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
-
-        /// <summary>
-        /// ContentPlaceHolder2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
