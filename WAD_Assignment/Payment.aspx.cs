f19a63293620eec0d878e34ff8a58f0dbb6b223e
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-02-22 19:29:46 +0800

    Update Payment (+3.0)

--- WAD_Assignment/Payment.aspx.cs
+++ WAD_Assignment/Payment.aspx.cs
@@ -102,7 +102,7 @@ namespace WAD_Assignment
             for (int i = 0; i < gvPayment.Rows.Count; i++)
             {
                 double tempP = double.Parse((gvPayment.Rows[i].FindControl("item_order_summary_price") as TextBox).Text.Trim());
-                double tempTP = double.Parse((gvPayment.Rows[i].FindControl("order_subtotal") as TextBox).Text.Trim());
+                double tempTP = double.Parse((gvPayment.Rows[i].FindControl("order_subtotal") as TextBox).Text.Trim()) + 3.0;
 
 
                 SqlCommand cmd = new SqlCommand("sp_createPayment", con);
