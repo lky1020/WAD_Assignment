d088ebb7c5b758a3321ca84583f9a87dae5cc0a0
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-04-11 12:59:38 +0800

    Update User Control (Go back to previous page)

--- WAD_Assignment/Login.aspx.cs
+++ WAD_Assignment/Login.aspx.cs
@@ -14,33 +14,18 @@ namespace WAD_Assignment
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            // If already got account exist, deactive it && delete the form authentication cookie
+
+            string previousUrl = Request.UrlReferrer.ToString();
+
+            // If unautorized access, go back to previous page
             if (Session["username"] != null)
             {
-                try
+
+                if (previousUrl != null)
                 {
-                    using (SqlConnection con = new SqlConnection(cs))
-                    {
-                        SqlCommand cmd = new SqlCommand("sp_ProfileDeactive", con);
-                        cmd.CommandType = CommandType.StoredProcedure;
-
-                        cmd.Parameters.AddWithValue("name", Session["username"].ToString());
-
-                        con.Open();
-                        cmd.ExecuteNonQuery();
-                        con.Close();
-                    }
-
-                    //Reset Authentication (Cookie)
-                    FormsAuthentication.SignOut();
-
-                    Response.Write("<script>alert('You been Direct to Login Page!');</script>");
+                    Response.Write("<script>alert('Unauthorized Access!!'); window.location = '" + previousUrl + "';</script>");
                 }
-                catch (Exception)
-                {
-                    ScriptManager.RegisterStartupScript(Page, this.GetType(), "LoginpageDBError", "alert('Error Occur in Database. Please Contact Quad-Core AWS!');", true);
-                    ScriptManager.RegisterStartupScript(Page, this.GetType(), "DirectToHomepage", "alert('Redirecting you to Homepage!'); window.location = 'Homepage.aspx';", true);
-                }
+
             }
 
         }
